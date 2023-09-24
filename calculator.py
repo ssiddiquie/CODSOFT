6f2eafede80fcdf8c103bcbc09285e270c0a8fb7
ssiddiquie <140342107+ssiddiquie@users.noreply.github.com>
2023-09-24 19:10:37 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,51 @@
-print("Calculator")
-print("Operations:")
-print("1- Add")
-print("2- Multiply")
-print("3- Division")
-print("4- Subtract")
+import tkinter as tk
+from tkinter import messagebox
 
-x = input("Press button 1-4  based on wanted operation\n")
 
-if x not in ['1', '2', '3', '4']:
-    print("Invalid")
-else:
-    a = float(input("Enter 1st number\n"))
-    b = float(input("Enter 2nd number:\n"))
+def calculate():
+    operation = operation_var.get()
+    num1 = float(entry_num1.get())
+    num2 = float(entry_num2.get())
 
-    if x == '1':
-        result = a + b
-    elif x == '2':
-        result = a * b
-    elif x == '3':
-        if b == 0:
-            print("Error: It cannot be divided by 0")
-            exit()
-        else:
-            result = a / b
-    else:
-        result = a - b
+    if operation == "Add":
+        result = num1 + num2
+    elif operation == "Multiply":
+        result = num1 * num2
+    elif operation == "Division":
+        if num2 == 0:
+            messagebox.showerror("Error", "Cannot divide by zero")
+            return
+        result = num1 / num2
+    elif operation == "Subtract":
+        result = num1 - num2
 
-    print(f"Result = {result}")
+    result_label.config(text=f"Result = {result}")
+
+
+app = tk.Tk()
+app.title("Calculator")
+
+operation_label = tk.Label(app, text="Select Operation:")
+operation_label.pack()
+operation_var = tk.StringVar()
+operation_var.set("Add")
+operation_option_menu = tk.OptionMenu(app, operation_var, "Add", "Multiply", "Division", "Subtract")
+operation_option_menu.pack()
+
+num1_label = tk.Label(app, text="Enter 1st number:")
+num1_label.pack()
+entry_num1 = tk.Entry(app)
+entry_num1.pack()
+
+num2_label = tk.Label(app, text="Enter 2nd number:")
+num2_label.pack()
+entry_num2 = tk.Entry(app)
+entry_num2.pack()
+
+calculate_button = tk.Button(app, text="Calculate", command=calculate)
+calculate_button.pack()
+
+result_label = tk.Label(app, text="")
+result_label.pack()
+
+app.mainloop()
